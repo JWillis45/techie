54a8fe17b3ecfb5507c42706c0558aaf8ca7c11a
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-11-02 17:22:24 -0800

    Setup Header
    Setup nav and space to display category title in the header

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+	<?php wp_head();?>
+</head>
+<body>
+  <div class="container">
+  	<div class="row header">
+  		
+  		<div class="col-xs-12 nav">
+  			
+  		<?php
+
+$defaults = array(
+    
+    'menu_class'      => 'nav nav-pills',
+    
+);
+
+
+wp_nav_menu( $defaults );
+
+?>
+<hr>
+  		</div> <!-- end of nav --> 
+    </div> <!-- end of header -->
+
+
+
+<div class="category">
+
+<h2><?php
+		if(( is_home() )){
+    		echo '<h2>- Start Your Adventure -<h2>';
+}
+		else{
+        echo the_category('','');
+}
+?>
+</h2>
+
+</h2>
+</div> <!-- end of category title -->
