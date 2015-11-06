b12bfa1394e1b2bf9925d2648e13fb0eaf519bcc
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-11-05 21:41:02 -0800

    Set up footer
    set up footer with an email field

--- footer.php
+++ footer.php
@@ -1,4 +1,31 @@
 	<div id="footer">
+<div class="row footer-content">
+	<div class="col-xs-12 footer-content">
+		<h2>Contact Us</h2>
+			<p>742 Evergreen Terrace <br>
+			   Springfield, Ca 91325<br>
+			   (661) 123-4567</p>
+	</div>
+
+	<div class="col-xs-12">
+		<h2>Subscribe</h2>
+	</div>
+	
+	<div class="email-form">
+	<?php echo do_shortcode( '[contact-form-7 id="62" title="Contact form 1" minlength:10 maxlength:100]
+
+' ); ?><!-- contact form -->
+	</div>
+	
+	<div class="col-xs-12 social-media">
+		<a href="https://instagram.com/?hl=en"><img src="<?php bloginfo('template_directory'); ?>/images/instagram-logo.png"></a>
+		<a href="https://twitter.com/?hl=en"><img src="<?php bloginfo('template_directory'); ?>/images/twitter-logo.png"></a>
+		<a href="https://facebook.com/?hl=en"><img src="<?php bloginfo('template_directory'); ?>/images/facebook-logo.png"></a>
+
+	</div>
+</div>
+
+
 		<?php wp_footer(); ?>
 	</div>
       
