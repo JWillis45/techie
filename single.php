1810db067c1ea7807c97200f7d97ed2d3f848c82
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-11-06 22:28:51 -0800

    added next and previous post buttons and banner ads

--- single.php
+++ single.php
@@ -15,27 +15,59 @@
      	
 
      </div>
-     </div><!-- end of row -->
+     </div><!-- end of row content-->
 
    <div class="row post-nav">
-   		<div class="col-xs-4 previous-post-button btn btn-primary">
-			<?php previous_post_link( '%link', 'Previous post', TRUE ); ?>
+   		<div class="col-xs-12 post-nav-content">
+   			<div class="col-xs-4 previous-post-button btn btn-primary">
+			<?php previous_post_link( '%link', 'Previous Post', TRUE ); ?>
 		
-		</div>
+			</div>
 		<div class="col-xs-4 post-button-category">
 			<p>| <?php the_category(', '); ?> |</p>
 		</div>
 		
 		<div class="col-xs-4 next-post-button btn btn-primary">
-		    <?php next_post_link( '%link', 'Next post', TRUE ); ?>
+		    <?php next_post_link( '%link', 'Next Post', TRUE ); ?>
 		</div>
+   		</div>
    </div>
-     <br>
+
+<?php endwhile; else: ?>
+	<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+     
+     <div class="row single-lower-ad">
+  
+   
+ <?php 
+
+  $args = array('post_type' => 'banners',
+                 'posts_per_page'=>'1',
+                 'orderby'       => 'rand');
+
+  $banners = new WP_Query( $args );
+
+
+  if ($banners -> have_posts()) : while ($banners -> have_posts()) : $banners -> the_post(); 
+
+  ?>
+ 
+
+  <div class="col-xs-12 banner-thumbs nopadding">
+	<div class="single-banner-content">
+    	<?php the_content();?>
+    </div>  
+  </div>
+
+</div>
+
+<?php endwhile; else: ?>
+    <p>Sorry, no pages matched your criteria.</p>
+<?php endif; wp_reset_postdata();?>
      
 		
 
-	<?php endwhile; else: ?>
-		<p>Sorry, no posts matched your criteria.</p>
-	<?php endif; ?>
+	
 
 <?php get_footer(); ?>
