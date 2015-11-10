d26e7eb7309f49f8b24a8e9d949ef4a43dd90108
JWillis45 <josephwillis@Josephs-MacBook-Pro.local>
2015-11-09 16:03:45 -0800

    setup advertise page

--- page-advertise.php
+++ page-advertise.php
@@ -4,14 +4,60 @@ Template Name: Advertise
 */
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="row entire-about nopadding">
+	<div class="row advertise-title">
+	<div class="col-xs-12 advertise-field">
+		<hr class="title-line-top">
+		<h1><?php the_field('advertise_title');?></h1>
+		<hr class="title-line-bottom">
+	</div>
+	</div>
+	
 
- 	<?php the_content(); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
+	<div class="col-xs-12 advertise-content">
+		<?php the_content(); ?>
+	</div>
+
+		  <?php endwhile; else: ?>
+		    <p>Sorry, no pages matched your criteria.</p>
+		  <?php endif; ?>
+	
+</div><!-- end of row entire-about -->
 
 
-  <?php endwhile; else: ?>
+<div class="row single-lower-ad">
+  
+   
+ 
+
+
+
+ <?php 
+
+  $args = array('post_type' => 'banners',
+                 'posts_per_page'=>'1',
+                 'orderby'       => 'rand');
+
+  $banners = new WP_Query( $args );
+
+
+  if ($banners -> have_posts()) : while ($banners -> have_posts()) : $banners -> the_post(); 
+
+  ?>
+ 
+
+  <div class="col-xs-12 banner-thumbs nopadding">
+  <div class="single-banner-content">
+      <?php the_content();?>
+    </div>  
+  </div>
+
+</div>
+
+<?php endwhile; else: ?>
     <p>Sorry, no pages matched your criteria.</p>
-<?php endif; ?>
-
+<?php endif; wp_reset_postdata();?>
 
 <?php get_footer(); ?>
